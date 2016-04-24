41ff409bf6c65f735db41afffc0219a66a75a1a6
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-04-24 16:16:03 -0600

    slow start

--- script.js
+++ script.js
@@ -1,60 +1,87 @@
-		
-		function Model() {
-            this.contacts = [];
-		}
-		
-		function Controller(model) {
-            this.createContact = function (first_name, last_name, email) {
-                this.first_name = first_name;
-                this.last_name = last_name;
-                this.email = email;
-                model.contacts.push(first_name, last_name, email);
-                
-            }
-                this.getAllContacts = function() {
-                return model.contacts;
-            }
-                view.showAllContacts();
-		}
-		//this is the end of the controller
-		function View(controller) { //this just updates the view
-            this.showAllContacts = function() {
-                for (var i = 0; i < controller.getAllPets().length; i++) {
-                    this.showContact(i);
-                }
-            }
-            this.showContact = function(index) {
-                var list = document.getElementById("list");
-                var contact = controller.getAllContacts()[index];
-                var p = document.createElement("div");
-                p.className = "contact";
-                p.innerHTML = contact.first_name + " -- " + contact.last_name + " -- " + contact.email;
-                list.appendChild(p);
-            }
-		}
+
+// constructor for  a Person
+function Person (firstName,lastName,email) {
+ this.first = firstName;
+ this.last = lastName;
+ this.email_address = address;
+}
 
 
-		
-		var m = new Model();
-		var c = new Controller(m);
-		var v = new View(c);
-		
-		document.getElementById("contacts-op-save").onclick = function() {
-            console.log("Clicky");
-			v.createContact();
-		}
+/*model
+* empty array of objects 
+*/
+function Model() {
+    this.people = [];
+    this.book = [];
+    
+}
+
+
+//controller takes in input from view
+//uses constructor function to create persons
+//adds it to the model array
+function Controller(model) {
+            
+            //function to add to array of objects
+            //function to add new Person object to array people and book
+			this.createPerson = function(firstName,lastName,email) 
+            {
+                var person = new Person(firstName,lastName,email);
+				model.people.push(person);
+                console.log(model.people);
+                model.book.push(person);
+			}
+
+              
+}
+//view function
+//takes in user input
+//sends to controller
+
+function View(controller)
+{
+    /* takes in input from html
+    * accepts value 
+    * passes value to controller
+    * then passes to constructor
+    */
+    this.createPerson = function()
+    {
+        var firstName = document.getElementById('fName').value;
+        var lastName = document.getElementById('lName').value;
+        var email = document.getElementById('p_email').value;
+        controller.createPerson(firstName,lastName,email);
         
+    }
+       
+    /*
+    *somehow make it so when a person/bussiness is added
+    *the display updates to show the person added to the address book
+    */
+    this.updateDisplay = function(Model)
+    {  
+        console.log(this.model.book);
+        for (i = 0; i < this.model.book.length; i++)
+        {
+        var para = document.createElement('P');
+        var div = document.createElement('Div');
+        para.appendChild('Div');
+        document.getElementById('pDisplay')
+        addressBook.appendChild(Model.book[i]);
         
-        Contacts.addEventListener("submit", function(event) {
-					var entry = {
-						id: parseInt(this.id_entry.value),
-						first_name: this.first_name.value,
-						last_name: this.last_name.value,
-						email: this.email.value
-					};
-            
-            
-            
-            
-            
-		v.showAllContacts();
+        }
+        
+    }
+   
+}
+
+
+var model = new Model();
+var controller = new Controller(model);
+var view = new View(controller);
+
+document.getElementById('personBtn').onclick = function()
+{
+    view.createPerson();
+    view.updateDisplay(model);
+}
